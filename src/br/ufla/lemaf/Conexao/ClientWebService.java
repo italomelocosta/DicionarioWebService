ef200059701ff00a849bec5787d3982a9df710c6
italomelocosta <italomelocosta@gmail.com>
2017-11-29 09:21:59 -0200

    Commit

---
+++ src/br/ufla/lemaf/Conexao/ClientWebService.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufla.lemaf.Conexao;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import javax.xml.ws.http.HTTPException;
+
+/**
+ *
+ * @author Italo
+ */
+public class ClientWebService {
+
+    String output = null;
+    String retorno;
+    ArrayList<String> lista = new ArrayList<String>();
+    //public static void main(String[] args) {
+
+    public String Pesquisa(int i) {
+
+        try {
+
+            URL url = new URL("http://testes.ti.lemaf.ufla.br/api/Dicionario/" + i);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            while ((output = br.readLine()) != null) {
+                    retorno = output;
+            }
+            
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        return retorno;
+    }
+}
